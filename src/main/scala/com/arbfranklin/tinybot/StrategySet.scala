333d0d69d8a428a1531058526d0a5fad689d4d4c
Andrew Franklin <arbfranklin@gmail.com>
2012-08-20 22:29:47 -0400

    Separated out strategy set and move selection into its own class

---
+++ src/main/scala/com/arbfranklin/tinybot/StrategySet.scala
@@ -0,0 +1,136 @@
+/*
+ * Copyright (c) 2012, Andrew Franklin
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.arbfranklin.tinybot
+
+import strategies.Strategy
+import util._
+import collection.GenIterable
+import util.Explode
+import util.SetState
+import util.Spawn
+import util.Status
+
+class StrategySet private(val strategies: List[(Strategy,Double)]) {
+  def add(tuple: (Strategy,Double)): StrategySet = new StrategySet((tuple._1, tuple._2) :: strategies)
+  def add(s: Strategy): StrategySet = new StrategySet((s, 1d) :: strategies)
+
+  /**evaluation handler */
+  def eval(ctx: ReactContext): List[Action] = {
+    // collate all the various actions that we could make
+    val actions = collateActions(ctx)
+
+    // explosions are the first priority
+    val explosions = actions.filter(_.action.isInstanceOf[Explode])
+    if (!explosions.isEmpty) {
+      // find the biggest requested explosion
+      val biggest = explosions.maxBy(_.action.asInstanceOf[Explode].size)
+      return List(biggest.action, Status(biggest.reason))
+    }
+
+    // collate any state setting
+    val states = actions.filter(_.action.isInstanceOf[SetState]).map(_.asInstanceOf[SetState]).toList
+
+    // now spawning, but no matter what we need a move location
+    val fm = findMove(actions)
+    val move = fm.action.asInstanceOf[Move]
+    val reason = fm.reason
+
+    // the move instructions
+    val moveInstructions = List(move, SetState("lastMove", move.toString))
+
+    // spawn?
+    val spawns = actions.filter(_.action.isInstanceOf[Spawn])
+    if (!spawns.isEmpty && fm.score.isPositive) {
+      // find the biggest spawn
+      val biggest = spawns.maxBy(_.action.asInstanceOf[Spawn].energy)
+
+      var spawn = biggest.action.asInstanceOf[Spawn]
+      if (spawn.direction != Move.Center) {
+        return List(spawn, Status(biggest.reason)) ::: moveInstructions ::: states
+      } else {
+        // where to spawn?
+        val moves = Move.values.filter(m => ctx.view.at(ctx.view.toXY(m))==Tile.Empty) - move
+        if (!moves.isEmpty) {
+          return List(Spawn(randFrom(moves), spawn.energy), Status(biggest.reason)) ::: moveInstructions ::: states
+        }
+      }
+      // unable to complete the spawn
+    }
+
+    // if we're here, it's just a regular move
+    Status(reason) :: moveInstructions ::: states
+  }
+
+  /**collate all the potential actions */
+  private def collateActions(ctx: ReactContext): GenIterable[Vote] = {
+    // TODO: apply parallel collections? Slower on my mac, but maybe not on a high-end server?
+    val allowedMoves = Move.values.filter(m => ctx.view.at(ctx.view.toXY(m)) != Tile.Wall).toSet
+
+    // iterate all the move suggestions the strategies have made
+    strategies.map(t => {
+      val strategy = t._1
+      val w = t._2
+
+      // obtain the candidates and re-weight them
+      val votes = strategy.eval(ctx, allowedMoves)
+      votes.map(v => Vote(v.action, v.score * w, v.reason))
+    }).flatten
+  }
+
+  /**find the best move in the action set */
+  private def findMove(actions: GenIterable[Vote]): Vote = {
+    // only interested in move actions at this point
+    val moves = actions.map(_.action).filter(_.isInstanceOf[Move])
+
+    // collaborate across the votes for each move square
+    val cscores = moves.map(move => {
+      val votes = actions.filter(_.action == move)
+      val score = Score.combine(votes.map(_.score))
+      val dominant = votes.maxBy(v => {
+        if (v.score == Score.Veto) Double.PositiveInfinity else v.score.d
+      }) // veto is dominant
+
+      // new colaborated vote
+      Vote(move, score, dominant.reason)
+    })
+
+    if (cscores.isEmpty) {
+      Vote(Move.Center, Score.Abstain, "*stuck*")
+    } else {
+      // TODO: the max score is the move, but not necessarily the reason
+      cscores.maxBy(_.score)
+    }
+  }
+
+  private def randFrom[T](list: List[T]): T = {
+    val rand = scala.util.Random
+    list(rand.nextInt(list.length))
+  }
+}
+
+object StrategySet {
+  def apply() = new StrategySet(List())
+}
--- src/main/scala/com/arbfranklin/tinybot/TinyBot.scala
+++ src/main/scala/com/arbfranklin/tinybot/TinyBot.scala
@@ -28,37 +28,34 @@ package com.arbfranklin.tinybot
 import strategies._
 import util._
 import util.Tile._
-import collection.GenIterable
 
 /**
  * TinyBot. Implementation of a Scalatron bot with a configuration managed by the provided Genome.
  */
 class TinyBot(val g: Genome) extends BotResponder {
   /**weighted strategies to use for reducing the choice of next move */
-  val masterStrategies = Map(
-    new AvoidEnergyLoss() -> g.master.avoidEnergyLoss.weight,
-    new FairHunter(Zugar) -> g.master.huntZugars.weight,
-    new FairHunter(Fluppet) -> g.master.huntFluppets.weight,
-    new CreateDistance(Wall) -> g.master.avoidWalls.weight,
-    new RandomMove() -> g.master.random.weight,
-    new VelocityMove() -> g.master.velocity.weight,
-    new StayForHomeComing(g.master.stay.minTurns) -> g.master.stay.weight,
-    new MasterSpawn(g.master.spawn.maxBots) -> 1d
-  ).toList
+  val masterStrategies = StrategySet()
+    .add(new MasterSpawn(g.master.spawn.maxBots))
+    .add(new AvoidEnergyLoss() -> g.master.avoidEnergyLoss.weight)
+    .add(new FairHunter(Zugar) -> g.master.huntZugars.weight)
+    .add(new FairHunter(Fluppet) -> g.master.huntFluppets.weight)
+    .add(new CreateDistance(Wall) -> g.master.avoidWalls.weight)
+    .add(new RandomMove() -> g.master.random.weight)
+    .add(new VelocityMove() -> g.master.velocity.weight)
+    .add(new StayForHomeComing(g.master.stay.minTurns) -> g.master.stay.weight)
 
-  val slaveStrategies = Map(
-    new AvoidEnergyLoss() -> g.slave.avoidEnergyLoss.weight,
-    new FairHunter(Zugar) -> g.slave.huntZugars.weight,
-    new FairHunter(Fluppet) -> g.slave.huntFluppets.weight,
-    new RandomMove() -> g.slave.random.weight,
-    new VelocityMove() -> g.slave.velocity.weight,
-    new ReturnSlaveHome(g.slave.home.roi, g.slave.home.safetyMargin, g.shared.spawn.maxBots) -> g.slave.home.weight,
-    new StuckKamakazi() -> 1d,
-    new EnemyChicken() -> 1d,
-    new ExplodeOnApocalypse(g.slave.apocalypse.minTurns) -> 1d,
-    new SlaveSpawn(g.slave.spawn.frequency, g.slave.spawn.imbalance, g.shared.spawn.maxBots) -> 1d,
-    new AttackBots(g.slave.attack.radius, g.slave.attack.minCount, g.slave.attack.maxEnergy) -> 1d
-  ).toList
+  val slaveStrategies = StrategySet()
+    .add(new StuckKamakazi())
+    .add(new EnemyChicken())
+    .add(new ExplodeOnApocalypse(g.slave.apocalypse.minTurns))
+    .add(new SlaveSpawn(g.slave.spawn.frequency, g.slave.spawn.imbalance, g.shared.spawn.maxBots))
+    .add(new AttackBots(g.slave.attack.radius, g.slave.attack.minCount, g.slave.attack.maxEnergy))
+    .add(new AvoidEnergyLoss() -> g.slave.avoidEnergyLoss.weight)
+    .add(new FairHunter(Zugar) -> g.slave.huntZugars.weight)
+    .add(new FairHunter(Fluppet) -> g.slave.huntFluppets.weight)
+    .add(new RandomMove() -> g.slave.random.weight)
+    .add(new VelocityMove() -> g.slave.velocity.weight)
+    .add(new ReturnSlaveHome(g.slave.home.roi, g.slave.home.safetyMargin, g.shared.spawn.maxBots) -> g.slave.home.weight)
 
   var startTime = 0L
 
@@ -83,12 +80,12 @@ class TinyBot(val g: Genome) extends BotResponder {
 
       print("\r[%4d] score=%d\r".format(ctx.time, ctx.energy))
     }
-    eval(ctx, masterStrategies)
+    masterStrategies.eval(ctx)
   }
 
   /**called when it's the slave's turn to react */
   override def reactAsSlave(ctx: SlaveContext): List[Action] = {
-    val actions = eval(ctx, slaveStrategies)
+    val actions = slaveStrategies.eval(ctx)
 
     // remove status updates that are too noisy
     actions.map(a => {
@@ -97,102 +94,5 @@ class TinyBot(val g: Genome) extends BotResponder {
         if (msg.length<=3) a else Status("")
       } else a
     })
-  }
-
-  //
-  // VOTE EVALUATION
-  //
-
-  /**evaluation handler */
-  def eval(ctx: ReactContext, strategies: List[(Strategy,Double)]): List[Action] = {
-    // collate all the various actions that we could make
-    val actions = collateActions(ctx, strategies)
-
-    // explosions are the first priority
-    val explosions = actions.filter(_.action.isInstanceOf[Explode])
-    if (!explosions.isEmpty) {
-      // find the biggest requested explosion
-      val biggest = explosions.maxBy(_.action.asInstanceOf[Explode].size)
-      return List(biggest.action, Status(biggest.reason))
-    }
-
-    // collate any state setting
-    val states = actions.filter(_.action.isInstanceOf[SetState]).map(_.asInstanceOf[SetState]).toList
-
-    // now spawning, but no matter what we need a move location
-    val fm = findMove(actions)
-    val move = fm.action.asInstanceOf[Move]
-    val reason = fm.reason
-
-    // the move instructions
-    val moveInstructions = List(move, SetState("lastMove", move.toString))
-
-    // spawn?
-    val spawns = actions.filter(_.action.isInstanceOf[Spawn])
-    if (!spawns.isEmpty && fm.score.isPositive) {
-      // find the biggest spawn
-      val biggest = spawns.maxBy(_.action.asInstanceOf[Spawn].energy)
-
-      var spawn = biggest.action.asInstanceOf[Spawn]
-      if (spawn.direction != Move.Center) {
-        return List(spawn, Status(biggest.reason)) ::: moveInstructions ::: states
-      } else {
-        // where to spawn?
-        val moves = Move.values.filter(m => ctx.view.at(ctx.view.toXY(m))==Tile.Empty) - move
-        if (!moves.isEmpty) {
-          return List(Spawn(randFrom(moves), spawn.energy), Status(biggest.reason)) ::: moveInstructions ::: states
-        }
-      }
-      // unable to complete the spawn
-    }
-
-    // if we're here, it's just a regular move
-    Status(reason) :: moveInstructions ::: states
-  }
-
-  /**collate all the potential actions */
-  def collateActions(ctx: ReactContext, strategies: List[(Strategy,Double)]): GenIterable[Vote] = {
-    // TODO: apply parallel collections? Slower on my mac, but maybe not on a high-end server?
-    val allowedMoves = Move.values.filter(m => ctx.view.at(ctx.view.toXY(m)) != Tile.Wall).toSet
-
-    // iterate all the move suggestions the strategies have made
-    strategies.map(t => {
-      val strategy = t._1
-      val w = t._2
-
-      // obtain the candidates and re-weight them
-      val votes = strategy.eval(ctx, allowedMoves)
-      votes.map(v => Vote(v.action, v.score * w, v.reason))
-    }).flatten
-  }
-
-  /**find the best move in the action set */
-  def findMove(actions: GenIterable[Vote]): Vote = {
-    // only interested in move actions at this point
-    val moves = actions.map(_.action).filter(_.isInstanceOf[Move])
-
-    // collaborate across the votes for each move square
-    val cscores = moves.map(move => {
-      val votes = actions.filter(_.action == move)
-      val score = Score.combine(votes.map(_.score))
-      val dominant = votes.maxBy(v => {
-        if (v.score == Score.Veto) Double.PositiveInfinity else v.score.d
-      }) // veto is dominant
-
-      // new colaborated vote
-      Vote(move, score, dominant.reason)
-    })
-
-    if (cscores.isEmpty) {
-      Vote(Move.Center, Score.Abstain, "*stuck*")
-    } else {
-      // TODO: the max score is the move, but not necessarily the reason
-      cscores.maxBy(_.score)
-    }
-  }
-
-  def randFrom[T](list: List[T]): T = {
-    val rand = scala.util.Random
-    list(rand.nextInt(list.length))
   }
 }
