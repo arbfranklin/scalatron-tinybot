9a21751a6bbf7e2a5a386b3f154b32270747b564
Andrew Franklin <arbfranklin@gmail.com>
2013-02-12 12:53:53 -0500

    Added optional arguments to play goal to allow headless runs + basic configuration

--- project/Build.scala
+++ project/Build.scala
@@ -21,19 +21,29 @@ object TinyBotBuild extends Build {
       "org.pegdown" % "pegdown" % "1.0.2" % "test",
       "junit" % "junit" % "4.7" % "test"),
 
+    // default parameters for scalatron play goal
     scalatronDir := file("/usr/local/scalatron"),
+    headless := false,
+    steps := 5000,
+    maxSlaves := 650,
 
-    play <<= (scalatronDir, name, javaOptions, pack in Compile) map {
-      (base, name, javaOptions, botJar) =>
-        require(base exists, "The setting '%s' must point to the base directory of an existing " +
-                "Scalatron installation.".format(scalatronDir.key.label))
+    play <<= (scalatronDir, name, javaOptions, pack in Compile, headless, steps, maxSlaves) map {
+      (base, name, javaOptions, botJar, headless, steps, maxSlaves) =>
+        require(base exists, "The setting '%s' must point to the base directory of an existing Scalatron installation.".format(scalatronDir.key.label))
         IO delete (base / "bots" / name)
         IO copyFile (botJar, base / "bots" / name / "ScalatronBot.jar")
-        Process("java" +: (javaOptions ++ Seq("-jar", "Scalatron.jar", "-browser", "no",
-          "-x", "100", "-y", "100", "-steps", "5000", "-maxslaves", "750", "-maxfps","1000")), base / "bin") !
+
+        val headlessOpts = if (headless) Seq("-maxfps", "1000", "-headless", "yes") else Seq()
+
+        Process("java" +: (javaOptions ++ Seq("-Ddebug=true", "-jar", "Scalatron.jar", "-browser", "no",
+          "-x", "100", "-y", "100", "-steps", steps.toString, "-maxslaves", maxSlaves.toString) ++ headlessOpts), base / "bin") !
     }
   )
 
+  val headless = SettingKey[Boolean]("headless", "whether the frontend should be started, or if we should just run in headless mode")
+  val maxSlaves = SettingKey[Int]("maxSlaves", "maximum number of mini-bots to allow per scalatron master")
+  val steps = SettingKey[Int]("steps", "number of simulation steps to make before the apocalypse")
   val scalatronDir = SettingKey[File]("scalatron-dir", "base directory of an existing Scalatron installation")
+
   val play = TaskKey[Unit]("play", "recompiles, packages and installs the bot, then starts Scalatron")
 }
